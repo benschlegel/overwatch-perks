2b47d3771dad2fbf2601313dfe57ddc8d61f5968
Ben Schlegel <ben5.schlegel@gmail.com>
2025-02-19 17:58:30 +0100

    chore: port tailwind config, add favicon + fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss';
+import type { PluginAPI } from 'tailwindcss/types/config';
 
-export default {
+const config: Config = {
 	darkMode: ['class'],
 	content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
 	theme: {
@@ -46,13 +47,83 @@ export default {
 					'4': 'hsl(var(--chart-4))',
 					'5': 'hsl(var(--chart-5))',
 				},
+				correct: 'hsl(var(--correct))',
+				incorrect: 'hsl(var(--incorrect))',
 			},
 			borderRadius: {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)',
 			},
+			scrollbarGutter: {
+				stable: 'stable',
+			},
+			keyframes: {
+				'accordion-down': {
+					from: {
+						height: '0',
+					},
+					to: {
+						height: 'var(--radix-accordion-content-height)',
+					},
+				},
+				'accordion-up': {
+					from: {
+						height: 'var(--radix-accordion-content-height)',
+					},
+					to: {
+						height: '0',
+					},
+				},
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+			},
 		},
 	},
-	plugins: [require('tailwindcss-animate')],
-} satisfies Config;
+	corePlugins: {
+		// Use custom aspect ratio plugin for better browser support
+		aspectRatio: false,
+	},
+	plugins: [
+		require('tailwindcss-animate'),
+		({ matchUtilities, theme /* … */ }: PluginAPI) => {
+			// …
+			matchUtilities(
+				// https://gist.github.com/olets/9b833a33d01384eed1e9f1e106003a3b
+				{
+					aspect: (value) => ({
+						'@supports (aspect-ratio: 1 / 1)': {
+							aspectRatio: value,
+						},
+						'@supports not (aspect-ratio: 1 / 1)': {
+							// https://github.com/takamoso/postcss-aspect-ratio-polyfill
+
+							'&::before': {
+								content: '""',
+								float: 'left',
+								paddingTop: `calc(100% / (${value}))`,
+							},
+							'&::after': {
+								clear: 'left',
+								content: '""',
+								display: 'block',
+							},
+						},
+					}),
+				},
+				{ values: theme('aspectRatio') }
+			);
+		},
+		({ addUtilities }: PluginAPI) => {
+			addUtilities({
+				'.scrollbar-stable': {
+					'scrollbar-gutter': 'stable',
+					// 'padding-right': 'calc(100vw - 100%)',
+				},
+			});
+		},
+	],
+};
+export default config;
