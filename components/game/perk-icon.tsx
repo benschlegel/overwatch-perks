c9a2422a136299c9a3c86b679bd3eb89acb269fc
Ben Schlegel <ben5.schlegel@gmail.com>
2025-02-21 17:02:21 +0100

    feat: add basic perk icon component

---
+++ components/game/perk-icon.tsx
@@ -0,0 +1,3 @@
+export default function PerkIcon() {
+	return <div className="rounded-full size-20 bg-secondary flex justify-center items-center">P</div>;
+}
--- components/landing-page/perk-info.tsx
+++ components/landing-page/perk-info.tsx
@@ -1,5 +1,7 @@
+import PerkIcon from '@/components/game/perk-icon';
 import HeroCard from '@/components/landing-page/hero-card';
 import { Card, CardContent } from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
 import { DEFAULT_HERO_ID } from '@/data/heroes';
 export default function PerkInfo() {
 	return (
@@ -7,8 +9,17 @@ export default function PerkInfo() {
 			<CardContent className="flex flex-col sm:gap-2 gap-1 sm:p-4 p-2 transition-colors">
 				<div className="flex items-center justify-between">
 					<HeroCard heroId={'ana'} />
-					<div className="flex w-full flex-col items-center justify-center">
+					<div className="flex w-full flex-col items-center justify-center gap-4">
 						<h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">Guess this perk</h2>
+						<PerkIcon />
+						<div className="w-full pl-4">
+							<Separator className="my-1" />
+							<div className="flex h-5 items-center space-x-4 text-sm text-muted-foreground w-full justify-center mt-2">
+								<div>Current Streak: 0</div>
+								<Separator orientation="vertical" />
+								<div>Best Streak: 0</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</CardContent>
