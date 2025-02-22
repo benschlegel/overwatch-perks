3e12e324742f017b76ea7c31064223938db65b2c
Ben Schlegel <ben5.schlegel@gmail.com>
2025-02-22 18:22:18 +0100

    feat: update help dialog description text

--- components/dialogs/help-dialog-content.tsx
+++ components/dialogs/help-dialog-content.tsx
@@ -41,10 +41,9 @@ export default function HelpContent({ setOpen }: Props) {
 				<main className="h-full w-full flex flex-col gap-6 px-2 pb-2 text-wrap break-words ">
 					{/* Description section */}
 					<blockquote className="sm:leading-7 tracking-wide opacity-90 border-l-[3px] pl-4 mt-1">
-						Guess the correct Overwatch League player within 8 attempts to win (inspired by wordle). After each guess, you will receive hints based on
-						attributes like the player's role, team, region and nationality to help you get closer to the right answer.
+						Guess the new Overwatch Perk based on the icon and four possible answers. Try to get the longest streak possible without getting a Perk wrong.
 					</blockquote>
-
+					<p>Description work in progress</p>
 					{/* Tips section */}
 					<div>
 						<div className="flex gap-2 items-center first:mt-0 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight">
@@ -68,17 +67,23 @@ export default function HelpContent({ setOpen }: Props) {
 						</div>
 						<div className="flex flex-col gap-2">
 							<p className="text-base tracking-normal mt-4">
-								All player data was sourced through <LinkButton href={'https://liquipedia.net/overwatch/Overwatch_League'}>Liquipedia</LinkButton>. Not
-								affiliated with Blizzard Entertainment.
+								All data was sourced through the game itself and{' '}
+								<LinkButton href={'https://docs.google.com/spreadsheets/d/1SGHStOwY3fuYuQ8yKwQVmY2YiUzNn_aCaC4VCn_DzEc/edit?gid=0#gid=0'}>
+									ObsSojourn's cheatsheet
+								</LinkButton>
+								. Not affiliated with Blizzard Entertainment.
 							</p>
+							<div>
+								<p className="scroll-m-20 text-base leading-7">Thanks to @Aerux for help with processing the perk icons!</p>
+							</div>
 							<div>
 								<p className="scroll-m-20 text-base leading-7">
 									If you like this project, you can{' '}
 									<LinkButton type="button" href="" onClick={() => setDialog('feedback')}>
 										send feedback/suggestions
 									</LinkButton>
-									, <LinkButton href={'https://ko-fi.com/scorer5'}>buy me a coffee</LinkButton> or check out the source code for this project on{' '}
-									<LinkButton href={'https://github.com/benschlegel/OWLDLE'}>Github</LinkButton>.
+									, <LinkButton href={'https://ko-fi.com/scorer5'}>donate</LinkButton> or check out the source code for this project on{' '}
+									<LinkButton href={'https://github.com/benschlegel/overwatch-perks'}>Github</LinkButton>.
 								</p>
 							</div>
 						</div>
