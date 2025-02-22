af3d627994e1003569ed85525d9acdd1e2b69e23
Ben Schlegel <ben5.schlegel@gmail.com>
2025-02-22 15:02:57 +0100

    style: improve go next button style for mobile

--- components/landing-page/controls.tsx
+++ components/landing-page/controls.tsx
@@ -16,9 +16,9 @@ export default function Controls() {
 					</div>
 				</div>
 			</Button>
-			<Button variant="secondary" disabled={gameState === 'in-progress'} onClick={rerollPerk}>
+			<Button variant="secondary" disabled={gameState === 'in-progress'} onClick={rerollPerk} className="sm:px-5 px-8">
 				<p>Next</p>
-				<kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+				<kbd className="pointer-events-none sm:inline-flex hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
 					C
 				</kbd>
 			</Button>
