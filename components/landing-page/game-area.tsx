7be69d3d877142081873d9d0c6285be614e23320
Ben Schlegel <ben5.schlegel@gmail.com>
2025-02-23 09:31:33 +0100

    fix: wrap game area in suspense to fix nuqs useSearchParams issue

--- components/landing-page/game-area.tsx
+++ components/landing-page/game-area.tsx
@@ -1,13 +1,16 @@
 import AnswerCards from '@/components/landing-page/answer-cards';
 import Controls from '@/components/landing-page/controls';
 import PerkInfo from '@/components/landing-page/perk-info';
+import { Suspense } from 'react';
 
 export default function GameArea() {
 	return (
 		<div className="flex flex-col gap-4">
-			<PerkInfo />
-			<AnswerCards />
-			<Controls />
+			<Suspense>
+				<PerkInfo />
+				<AnswerCards />
+				<Controls />
+			</Suspense>
 		</div>
 	);
 }
