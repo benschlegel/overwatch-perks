e65f98024ff8ddb37afddbd7b77ce2ba706caf05
Ben Schlegel <ben5.schlegel@gmail.com>
2025-02-23 18:00:37 +0100

    style: improve + unify link button styling

--- components/landing-page/donate-text.tsx
+++ components/landing-page/donate-text.tsx
@@ -5,7 +5,7 @@ export default function DonateText() {
 		<div className="p-2 px-4 opacity-60 w-full flex items-center justify-center">
 			<p className="text-sm text-muted-foreground text-center">
 				If you enjoy the game and want to help with server costs or support me, you can do so{' '}
-				<LinkButton href={'https://ko-fi.com/scorer5'} className="text-sm text-muted-foreground text-center">
+				<LinkButton href={'https://ko-fi.com/scorer5'} className="text-sm  text-muted-foreground text-center !p-0">
 					here
 				</LinkButton>
 				.
--- components/ui/link-button.tsx
+++ components/ui/link-button.tsx
@@ -1,5 +1,6 @@
 import { EnhancedButton } from '@/components/ui/enhanced-button';
 import Link from 'next/link';
+import { cn } from '@/lib/utils';
 import type { HtmlHTMLAttributes, PropsWithChildren } from 'react';
 
 interface Props extends HtmlHTMLAttributes<HTMLDivElement> {
@@ -11,19 +12,19 @@ interface Props extends HtmlHTMLAttributes<HTMLDivElement> {
 const className: React.ComponentProps<'a'>['className'] =
 	'text-base tracking-normal rounded-lg px-[0.1rem] py-[0.1rem] focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-primary-foreground focus-visible:ring-offset-1 p-0!';
 
-export default function LinkButton({ children, href, type = 'a', onClick, className: cn }: PropsWithChildren<Props>) {
+export default function LinkButton({ children, href, type = 'a', onClick, className: passedClassName }: PropsWithChildren<Props>) {
 	return (
-		<EnhancedButton variant={'linkHover1'} tabIndex={-1} onClick={onClick} className={cn}>
+		<EnhancedButton variant={'linkHover1'} tabIndex={-1} onClick={onClick} className={cn(passedClassName, className)}>
 			{type === 'Link' ? (
-				<Link href={href} className={className}>
+				<Link href={href} className={cn(className, passedClassName)}>
 					{children}
 				</Link>
 			) : type === 'a' ? (
-				<a href={href} className={className} target="_blank" rel="noreferrer">
+				<a href={href} className={cn(className, passedClassName)} target="_blank" rel="noreferrer">
 					{children}
 				</a>
 			) : (
-				<div className={className}>{children}</div>
+				<div className={cn(className, passedClassName)}>{children}</div>
 			)}
 		</EnhancedButton>
 	);
