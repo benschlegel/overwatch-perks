e3995e4c3d5e1d167bd3ec370cd024ec2cdaa285
Ben Schlegel <ben5.schlegel@gmail.com>
2025-02-23 16:58:27 +0100

    style: improve answer card focus style

--- components/landing-page/perk-card.tsx
+++ components/landing-page/perk-card.tsx
@@ -28,7 +28,7 @@ export default function PerkCard({ perk, index, className, correctPerkId }: Prop
 		<Card
 			data-correct={result}
 			className={cn(
-				'w-full outline-background-primary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 data-[correct=false]:outline-none data-[correct=false]:ring-2 data-[correct=false]:ring-ring data-[correct=false]:ring-offset-2 ring-offset-background hover:bg-secondary focus-visible:bg-secondary focus-visible:transition-none transition-colors data-[correct=false]:bg-red-600/45 data-[correct=true]:bg-green-600/45',
+				'w-full outline-background-primary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 data-[correct=false]:outline-none data-[correct=false]:ring-2 data-[correct=false]:ring-ring data-[correct=false]:ring-offset-1 ring-offset-background hover:bg-secondary focus-visible:bg-secondary focus-visible:transition-none transition-colors data-[correct=false]:bg-red-600/45 data-[correct=true]:bg-green-600/45',
 				className
 			)}
 			tabIndex={0}
