67e6a8ec2e63f35357fc67e64e5c6cc026b2efff
Ben Schlegel <ben5.schlegel@gmail.com>
2025-02-28 16:32:30 +0100

    style: center perk description + experiment with perk icon

--- app/cheatsheet/components/perk-card.tsx
+++ app/cheatsheet/components/perk-card.tsx
@@ -1,3 +1,4 @@
+import PerkIcon from '@/components/game/perk-icon';
 import { Card, CardContent } from '@/components/ui/card';
 import HighlightText from '@/components/ui/highlight-text';
 import type { Perk } from '@/data/perks';
@@ -16,10 +17,21 @@ export default function PerkCardBase({ className, perk, ...props }: Props) {
 				'w-full outline-background-primary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
 				className
 			)}>
-			<CardContent className="flex flex-col h-full text-center sm:gap-2 gap-1 sm:p-4 p-2 transition-colors">
+			<CardContent className="relative flex flex-col h-full text-center sm:gap-2 gap-1 sm:p-4 p-2 !pt-6 transition-colors">
+				{/* <div className="flex w-full items-center justify-center">
+					<PerkIcon perk={perk} className="size-12" />
+				</div> */}
+				<div className="absolute top-2 right-2">
+					<PerkIcon perk={perk} className="size-12" />
+				</div>
+				{/* <div className="absolute top-2 left-2">
+					<PerkIcon perk={perk} className="size-12" />
+				</div> */}
 				<div className="flex flex-col flex-1">
 					<p className="font-semibold sm:text-lg text-base">{perk.name}</p>
-					<HighlightText className="sm:text-base text-sm mt-[0.4rem] sm:mt-1" text={perk.description} />
+					<div className="flex flex-1 items-center justify-center">
+						<HighlightText className="sm:text-base text-sm mt-[0.4rem] sm:mt-2" text={perk.description} />
+					</div>
 				</div>
 			</CardContent>
 		</Card>
