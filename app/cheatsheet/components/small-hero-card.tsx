eff8027cd695e62266564ea3609e09dac6d40ad3
Ben Schlegel <ben5.schlegel@gmail.com>
2025-02-28 18:38:55 +0100

    style: improve hero select animation

--- app/cheatsheet/components/small-hero-card.tsx
+++ app/cheatsheet/components/small-hero-card.tsx
@@ -10,7 +10,7 @@ type Props = {
 export default function SmallHeroCard({ hero }: Props) {
 	return (
 		<Link href={`/cheatsheet/${hero.id}`} className="transform transition-transform hover:scale-125 hover:z-[9999] ">
-			<div className="bg-secondary rounded-lg flex flex-col justify-center items-center hover:outline-none border-none hover:ring-2 hover:ring-secondary-foreground ring-offset-transparent hover:ring-offset-1">
+			<div className="bg-secondary rounded-lg flex flex-col justify-center items-center hover:outline-none border-none hover:ring-2 hover:ring-border ring-offset-transparent hover:ring-offset-1">
 				<SmallHeroImage hero={hero} />
 				<Separator className="mb-2 h-1 transition-colors duration-300" />
 				<div className="flex flex-col items-center justify-center">
