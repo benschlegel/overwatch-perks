6c4e9d77c8967a5cb8c99b71f8b4aac3ac993bc4
Ben Schlegel <ben5.schlegel@gmail.com>
2025-02-28 16:10:11 +0100

    feat: add link to hero image + footer button

--- app/cheatsheet/components/cheatsheet-hero-image.tsx
+++ app/cheatsheet/components/cheatsheet-hero-image.tsx
@@ -1,6 +1,7 @@
 import LinkHeroImage from '@/app/cheatsheet/components/cheatsheet-hero-img';
 import { Separator } from '@/components/ui/separator';
 import { getHeroName, type HeroId } from '@/data/heroes';
+import Link from 'next/link';
 
 type Props = {
 	heroId: HeroId;
@@ -10,12 +11,14 @@ export default function CheatsheetHeroCard({ heroId }: Props) {
 	const heroName = getHeroName(heroId);
 
 	return (
-		<div className="bg-secondary max-w-[30rem] max-h-[50rem] rounded-lg flex flex-col justify-center items-center">
-			<LinkHeroImage heroId={heroId} />
-			<Separator className="mb-2 h-1 transition-colors duration-300" />
-			<div className="flex flex-col items-center justify-center">
-				<p className="pb-2 font-bold font-mono text-center">{heroName ?? 'loading'}</p>
+		<Link href={'/cheatsheet'}>
+			<div className="bg-secondary max-w-[30rem] max-h-[50rem] rounded-lg flex flex-col justify-center items-center hover:outline-none border-none hover:ring-1 hover:ring-secondary-foreground ring-offset-transparent hover:ring-offset-1">
+				<LinkHeroImage heroId={heroId} />
+				<Separator className="mb-2 h-1 transition-colors duration-300" />
+				<div className="flex flex-col items-center justify-center">
+					<p className="pb-2 font-bold font-mono text-center">{heroName ?? 'loading'}</p>
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 }
--- app/cheatsheet/components/hero-name-button.tsx
+++ app/cheatsheet/components/hero-name-button.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { RefreshCwIcon } from 'lucide-react';
+import Link from 'next/link';
 
 type Props = {
 	heroName?: string;
@@ -7,11 +8,14 @@ type Props = {
 
 export default function HeroNameButton({ heroName }: Props) {
 	return (
-		<Button variant="ghost" className="h-auto" aria-label="Change hero">
-			<h1 className="scroll-m-20 sm:text-4xl text-3xl font-bold tracking-tight text-center pb-[0.35rem] whitespace-break-spaces">
-				{heroName} <RefreshCwIcon className="!size-[1.2rem] sm:ml-[0.35rem] ml-1 inline !transition-all" />
-			</h1>
-			<span className="sr-only">Change hero</span>
-		</Button>
+		<Link href={'/cheatsheet'}>
+			<Button variant="ghost" className="h-auto" aria-label="Change hero">
+				<h1 className="scroll-m-20 sm:text-4xl text-3xl font-bold tracking-tight text-center pb-[0.35rem] whitespace-break-spaces">
+					{heroName}
+					{/* {heroName} <RefreshCwIcon className="!size-[1.2rem] sm:ml-[0.35rem] ml-1 inline !transition-all" /> */}
+				</h1>
+				<span className="sr-only">Change hero</span>
+			</Button>
+		</Link>
 	);
 }
