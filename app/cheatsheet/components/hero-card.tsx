660c27e4e7700357c3a19fd8c60070e135bd8fcd
Ben Schlegel <ben5.schlegel@gmail.com>
2025-02-28 11:38:38 +0100

    refactor: move hero name button in its own component

--- app/cheatsheet/components/hero-card.tsx
+++ app/cheatsheet/components/hero-card.tsx
@@ -1,11 +1,10 @@
 import CheatsheetHeroCard from '@/app/cheatsheet/components/cheatsheet-hero-image';
+import HeroNameButton from '@/app/cheatsheet/components/hero-name-button';
 import type { TabKey } from '@/app/cheatsheet/hooks/use-tab-param';
-import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
 import { TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { getHeroName, type HeroId } from '@/data/heroes';
-import { RefreshCwIcon } from 'lucide-react';
 
 type Props = {
 	heroId: HeroId;
@@ -21,13 +20,7 @@ export default function CheatsheetHeroCardWrapper({ heroId }: Props) {
 					<div className="flex w-full flex-col items-center justify-center">
 						<h2 className="scroll-m-20 border-b pb-1 sm:text-3xl text-xl font-semibold tracking-tight first:mt-0">Perk Info</h2>
 						<div className="flex-1 flex items-center">
-							<Button variant="ghost" className="h-auto" aria-label="Help">
-								<h1 className="scroll-m-20 sm:text-4xl text-3xl font-bold tracking-tight text-center pb-[0.35rem] whitespace-break-spaces">
-									{heroName} <RefreshCwIcon className="!size-[1.2rem] sm:ml-[0.35rem] ml-1 inline !transition-all" />
-								</h1>
-								{/* <RefreshCwIcon className="!size-[1.2rem] !transition-all" /> */}
-								<span className="sr-only">Change hero</span>
-							</Button>
+							<HeroNameButton heroName={heroName} />
 						</div>
 						{/* <Link href="/cheatsheet">
 							<Button className="bg-primary-foreground text-white opacity-85 hover:bg-primary-foreground hover:opacity-100">Switch hero</Button>
---
+++ app/cheatsheet/components/hero-name-button.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@/components/ui/button';
+import { RefreshCwIcon } from 'lucide-react';
+
+type Props = {
+	heroName?: string;
+};
+
+export default function HeroNameButton({ heroName }: Props) {
+	return (
+		<Button variant="ghost" className="h-auto" aria-label="Change hero">
+			<h1 className="scroll-m-20 sm:text-4xl text-3xl font-bold tracking-tight text-center pb-[0.35rem] whitespace-break-spaces">
+				{heroName} <RefreshCwIcon className="!size-[1.2rem] sm:ml-[0.35rem] ml-1 inline !transition-all" />
+			</h1>
+			<span className="sr-only">Change hero</span>
+		</Button>
+	);
+}
