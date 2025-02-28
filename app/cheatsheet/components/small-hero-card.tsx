4477e0835b033d2a82b4ff93e0f88d6c67515f5e
Ben Schlegel <ben5.schlegel@gmail.com>
2025-02-28 18:35:40 +0100

    feat: add scale animation + link functionality to hero select

--- app/cheatsheet/components/small-hero-card.tsx
+++ app/cheatsheet/components/small-hero-card.tsx
@@ -1,6 +1,7 @@
 import SmallHeroImage from '@/app/cheatsheet/components/small-hero-img';
 import { Separator } from '@/components/ui/separator';
 import type { Hero } from '@/data/heroes';
+import Link from 'next/link';
 
 type Props = {
 	hero: Hero;
@@ -8,12 +9,14 @@ type Props = {
 
 export default function SmallHeroCard({ hero }: Props) {
 	return (
-		<div className="bg-secondary rounded-lg flex flex-col justify-center items-center">
-			<SmallHeroImage hero={hero} />
-			<Separator className="mb-2 h-1 transition-colors duration-300" />
-			<div className="flex flex-col items-center justify-center">
-				<p className="pb-2 font-bold font-mono">{hero.name ?? 'loading'}</p>
+		<Link href={`/cheatsheet/${hero.id}`} className="transform transition-transform hover:scale-125 hover:z-[9999] ">
+			<div className="bg-secondary rounded-lg flex flex-col justify-center items-center hover:outline-none border-none hover:ring-2 hover:ring-secondary-foreground ring-offset-transparent hover:ring-offset-1">
+				<SmallHeroImage hero={hero} />
+				<Separator className="mb-2 h-1 transition-colors duration-300" />
+				<div className="flex flex-col items-center justify-center">
+					<p className="pb-2 font-bold font-mono">{hero.name ?? 'loading'}</p>
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 }
