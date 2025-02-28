6a153d84e9f7b84e6aa6450f3f51ba7650adc226
Ben Schlegel <ben5.schlegel@gmail.com>
2025-02-28 17:21:33 +0100

    style: unify perk card styling

--- app/cheatsheet/components/perk-card.tsx
+++ app/cheatsheet/components/perk-card.tsx
@@ -11,24 +11,27 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 
 export default function PerkCardBase({ className, perk, ...props }: Props) {
 	if (perk === undefined) return <></>;
+	// TODO: convert to grid and have 4 row layout on mobile
 	return (
 		<Card
 			className={cn(
 				'w-full outline-background-primary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
 				className
 			)}>
-			<CardContent className="relative flex flex-col h-full text-center sm:gap-2 gap-1 sm:p-4 p-2 !pt-6 transition-colors">
+			<CardContent className="relative flex flex-col h-full text-center sm:gap-2 gap-1 sm:p-4 p-2 transition-colors">
 				{/* <div className="flex w-full items-center justify-center">
 					<PerkIcon perk={perk} className="size-12" />
 				</div> */}
-				<div className="absolute top-2 right-2">
+				{/* <div className="absolute top-2 right-2">
 					<PerkIcon perk={perk} className="size-12" />
-				</div>
+				</div> */}
 				{/* <div className="absolute top-2 left-2">
 					<PerkIcon perk={perk} className="size-12" />
 				</div> */}
 				<div className="flex flex-col flex-1">
-					<p className="font-semibold sm:text-lg text-base">{perk.name}</p>
+					<div className="flex w-full items-center justify-center">
+						<p className="font-semibold sm:text-lg text-base">{perk.name}</p>
+					</div>
 					<div className="flex flex-1 items-center justify-center">
 						<HighlightText className="sm:text-base text-sm mt-[0.4rem] sm:mt-2" text={perk.description} />
 					</div>
