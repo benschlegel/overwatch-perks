18893e3f4238af844857f4b2b1bafc71402acd58
Ben Schlegel <ben5.schlegel@gmail.com>
2025-02-28 17:30:47 +0100

    style: align card content to center

--- app/cheatsheet/components/perk-card.tsx
+++ app/cheatsheet/components/perk-card.tsx
@@ -18,22 +18,20 @@ export default function PerkCardBase({ className, perk, ...props }: Props) {
 				'w-full outline-background-primary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
 				className
 			)}>
-			<CardContent className="relative flex flex-col h-full text-center sm:gap-2 gap-1 sm:p-4 p-2 transition-colors">
+			<CardContent className="relative flex items-center justify-center flex-col h-full text-center sm:gap-2 gap-1 sm:p-4 p-2 transition-colors">
 				{/* <div className="flex w-full items-center justify-center">
 					<PerkIcon perk={perk} className="size-12" />
 				</div> */}
-				{/* <div className="absolute top-2 right-2">
-					<PerkIcon perk={perk} className="size-12" />
-				</div> */}
+				<div className="absolute top-2 right-2">
+					<PerkIcon perk={perk} className="size-11" />
+				</div>
 				{/* <div className="absolute top-2 left-2">
 					<PerkIcon perk={perk} className="size-12" />
 				</div> */}
-				<div className="flex flex-col flex-1">
-					<div className="flex w-full items-center justify-center">
-						<p className="font-semibold sm:text-lg text-base">{perk.name}</p>
-					</div>
+				<div className="flex flex-col">
+					<p className="font-semibold sm:text-lg text-base">{perk.name}</p>
 					<div className="flex flex-1 items-center justify-center">
-						<HighlightText className="sm:text-base text-sm mt-[0.4rem] sm:mt-2" text={perk.description} />
+						<HighlightText className="sm:text-base text-sm mt-[0.4rem] sm:mt-3" text={perk.description} />
 					</div>
 				</div>
 			</CardContent>
