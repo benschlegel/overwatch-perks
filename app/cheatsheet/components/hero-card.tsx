8207c55e75b69f86fe663107c7b45fe6b0935986
Ben Schlegel <ben5.schlegel@gmail.com>
2025-02-28 10:13:21 +0100

    style: fix hero card styling

--- app/cheatsheet/components/hero-card.tsx
+++ app/cheatsheet/components/hero-card.tsx
@@ -14,18 +14,18 @@ export default function CheatsheetHeroCardWrapper({ heroId }: Props) {
 	return (
 		<Card className="transition-colors">
 			<CardContent className="flex flex-col sm:gap-2 gap-1 sm:p-4 p-2 transition-colors">
-				<div className="flex items-center justify-between">
+				<div className="flex items-stretch justify-between gap-4">
 					<CheatsheetHeroCard heroId={heroId} />
 					<div className="flex w-full flex-col items-center justify-center gap-4">
-						<h2 className="scroll-m-20 border-b pb-2 sm:text-3xl text-xl font-semibold tracking-tight first:mt-0">Perk Info</h2>
-						<h1 className="scroll-m-20 sm:text-4xl text-3xl font-bold tracking-tight sm:py-[0.85rem] pt-1 px-4 text-center flex-1">{heroName}</h1>
+						<h2 className="scroll-m-20 border-b sm:text-3xl text-xl font-semibold tracking-tight first:mt-0">Perk Info</h2>
+						<h1 className="scroll-m-20 sm:text-4xl text-3xl font-bold tracking-tight text-center flex-1 flex items-center">{heroName}</h1>
 						{/* <Link href="/cheatsheet">
 							<Button className="bg-primary-foreground text-white opacity-85 hover:bg-primary-foreground hover:opacity-100">Switch hero</Button>
 						</Link>  */}
-						<div className="w-full pl-4">
-							<Separator className="mt-1 " />
-							<div className="flex items-center sm:space-x-4 sm:text-sm text-xs text-muted-foreground w-full justify-center mt-2 sm:mb-[-0.4rem] ">
-								<TabsList className="flex w-full sm:mx-2">
+						<div className="w-full flex flex-col gap-3">
+							<Separator />
+							<div className="flex items-center sm:space-x-4 sm:text-sm text-xs text-muted-foreground w-full justify-center">
+								<TabsList className="flex w-full ">
 									<TabsTrigger className="flex-1" value={'info' as TabKey}>
 										Info
 									</TabsTrigger>
