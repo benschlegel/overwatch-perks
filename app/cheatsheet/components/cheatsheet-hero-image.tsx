a717c28665052031bb4201d7f372536e199bf20f
Ben Schlegel <ben5.schlegel@gmail.com>
2025-03-01 09:10:06 +0100

    feat: keep tab state across navigation + fix hero card dialog open

--- app/cheatsheet/components/cheatsheet-hero-image.tsx
+++ app/cheatsheet/components/cheatsheet-hero-image.tsx
@@ -1,7 +1,8 @@
 import LinkHeroImage from '@/app/cheatsheet/components/cheatsheet-hero-img';
+import { HeroSelectContext } from '@/app/cheatsheet/context/HeroSelectContext';
 import { Separator } from '@/components/ui/separator';
 import { getHeroName, type HeroId } from '@/data/heroes';
-import Link from 'next/link';
+import { useCallback, useContext } from 'react';
 
 type Props = {
 	heroId: HeroId;
@@ -9,16 +10,28 @@ type Props = {
 
 export default function CheatsheetHeroCard({ heroId }: Props) {
 	const heroName = getHeroName(heroId);
+	const { setIsOpen } = useContext(HeroSelectContext);
 
+	const onClick = useCallback(() => {
+		setIsOpen(true);
+	}, [setIsOpen]);
 	return (
-		<Link href={'/cheatsheet'}>
-			<div className="bg-secondary max-w-[30rem] max-h-[50rem] rounded-lg flex flex-col justify-center items-center hover:outline-none border-none hover:ring-1 hover:ring-secondary-foreground ring-offset-transparent hover:ring-offset-1">
-				<LinkHeroImage heroId={heroId} />
-				<Separator className="mb-2 h-1 transition-colors duration-300" />
-				<div className="flex flex-col items-center justify-center">
-					<p className="pb-2 font-bold font-mono text-center">{heroName ?? 'loading'}</p>
-				</div>
+		<div
+			className="bg-secondary max-w-[30rem] max-h-[50rem] rounded-lg flex flex-col justify-center items-center hover:outline-none border-none hover:ring-1 hover:ring-secondary-foreground ring-offset-transparent hover:ring-offset-1"
+			tabIndex={0}
+			onClick={onClick}
+			// biome-ignore lint/a11y/useSemanticElements: easier to keep card styling this way
+			role="button"
+			onKeyDown={(e) => {
+				if (e.key === 'Enter' || e.key === ' ') {
+					onClick();
+				}
+			}}>
+			<LinkHeroImage heroId={heroId} />
+			<Separator className="mb-2 h-1 transition-colors duration-300" />
+			<div className="flex flex-col items-center justify-center">
+				<p className="pb-2 font-bold font-mono text-center">{heroName ?? 'loading'}</p>
 			</div>
-		</Link>
+		</div>
 	);
 }
